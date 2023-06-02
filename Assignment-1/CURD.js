28217c1cb0207adbd237de3750d8a2e90e6b6ba7
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-06-03 04:01:36 +0530

    Adding curd opration.

---
+++ Assignment-1/CURD.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const mysql = require('mysql');
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const bodyPareser = require('body-parser');
+
+
+const app = express();
+app.use(bodyPareser.json());
+
+const connction = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.PASSWORD,
+  database: 'users'
+})
+
+connction.connect(err => {
+  if (err) {
+    new Error(`DB conncetion has failed :-(`)
+  }
+  console.log(`DB connection has made sucessfully :-)`);
+})
+
+const handleRequest = (req, res) => {
+  connction.end((err => {
+    if (err) {
+      res.send(`Something worng in conncetion :-(`)
+    }
+    res.send(`Enjoy yourself :-)`);
+  }))
+}
+
+// User registration: 
+app.post('/register', (req, res) => {
+  const { username, password, email } = req.body;
+
+  // Hash the password..
+  bcrypt.hash(password, 10, (err, hashPassword) => {
+    if (err) {
+      console.error(`Error occure hasing the password!`, err);
+      res.status(500).json({
+        messgae: "Internal server error :-("
+      })
+      return;
+    }
+    const sql = `INSERT INTO users (username, password,email) VALUES (?,?,?)`
+    connction.query(sql, [username, hashPassword, email], (err, result) => {
+      if (err) {
+        console.error(`Error occure hato create user :-(`, err);
+        res.status(500).json({
+          messgae: "Internal server error :-("
+        })
+        return;
+
+      }
+      res.status(201).json({
+        messgae: `User create sucessfully :-)`
+      })
+    })
+  })
+})
+
+app.get('/', (req, res) => {
+  res.send("<h2>This the home page</h2>")
+})
+
+PORT = 8000;
+app.listen(PORT, () => {
+  console.log("Am Listing Master Kanha :-)");
+})
+
--- Assignment-1/server.js
+++ Assignment-1/server.js
@@ -1,37 +1,92 @@
 const express = require('express');
 const mysql = require('mysql');
 require('dotenv').config();
+const bodyParser = require('body-parser');
 
-// CONNECTION TO THE MYSQL DB..
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: process.env.PASSWORD,
-  database: "users",
+  database: process.env.DATABASE
 })
 
 connection.connect((err) => {
   if (err) {
-    console.error(`The MySQL coonection if failed!`);
+    console.error('Failed to establish the Connection :(');
   }
-  console.log(`connected to MySQL DB:)`);
+  else {
+    console.log("Sucessfully establish the connction :)");
+  }
 })
 
-
-connection.end((err) => {
+connection.end((err => {
   if (err) {
-    console.error(`Error closing MySQL connection: `, err);
+    res.send('We are failed to connecting the DB :(');
   }
-  console.log(`sucessfully closed the connection:)`);
+  else {
+    if (err) {
+      res.send('We have made Sucessfully connecting to the DB :)');
+    }
+  }
+}))
+
+
+
+
+
+
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(bodyParser.json());
+
+// Serve the HTML form
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// POST route for form submission
+app.post('/login', (req, res) => {
+  // Retrieve form data from the request body
+  const { username, password } = req.body;
+
+  // Perform basic validation
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Please fill in all fields.' });
+  }
+
+  // Do something with the form data (e.g., validate credentials, authenticate user)
+  // Auntication some pagination some more advance sorting and some user validation or something..
+  // ...
+
+  // Return a success response
+  res.json({ message: 'Login successful!' });
+});
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send("<h1>Welcome to Bagh's Land</h1>")
 })
 
-const app = express();
-
-app.get('/user', (req, res) => {
-  res.send("<h1>Hello kanha<h1>")
+app.get('/users', (req, res) => {
+  res.send('This is a response in the user page!!')
 })
 
 const PORT = 8000;
 app.listen(PORT, () => {
-  console.log("The port is listing!!");
+  console.log("Server is listing!!!");
 })
